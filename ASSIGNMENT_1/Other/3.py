228c77ef7e4df3b989f85d9fcf21147b7b7e3f0a
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2024-05-24 12:30:08 +0530

    Delete ASSIGNMENT_1/Other directory

--- ASSIGNMENT_1/Other/3.py
+++
@@ -1,9 +0,0 @@
-# The Iris dataset is typically used for 
-# classification tasks, but you can adapt 
-# the operations for regression or 
-# classification based on your specific goal.
-# You would need to modify the output layer
-# and loss accordingly. The softmax layer 
-# and cross-entropy loss are commonly 
-# used for multi-class classification.
-
